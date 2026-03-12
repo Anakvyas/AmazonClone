db7a02a15a6599bee33682be0f8f4fccb1fce5bc
ANAK VYAS <anakvyas@ANAKs-MacBook-Air.local>
2026-03-12 18:21:38 +0530

    modifying image to image[] for products

--- Backend/prisma/seed.js
+++ Backend/prisma/seed.js
@@ -3,33 +3,36 @@ const axios = require("axios")
 
 async function main() {
 
-  const response = await axios.get("https://dummyjson.com/products?limit=100")
+  const response = await axios.get("https://dummyjson.com/products")
 
   const products = response.data.products
 
   for (const p of products) {
 
-    // find category
     let category = await prisma.category.findUnique({
       where: { name: p.category }
     })
 
-    // create category if not exists
     if (!category) {
       category = await prisma.category.create({
         data: { name: p.category }
       })
     }
 
-    // create product
     await prisma.product.create({
       data: {
         name: p.title,
         description: p.description,
         price: p.price,
         stock: p.stock,
-        imageUrl: p.thumbnail,
-        categoryId: category.id
+        categoryId: category.id,
+
+        images: {
+          create: p.images.map((img) => ({
+            url: img
+          }))
+        }
+
       }
     })
 
